a84e808b7463819fcf912beb45cd1f5ef4354826
fuquanqian <100952620@qq.com>
2018-08-16 16:59:18 +0800

    000

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <mheader></mheader>
+    <tab></tab>
+    <route-view></route-view>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import Mheader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  components: {
+    Mheader,
+    Tab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable.styl"
+#app {
+  color: $color-theme
+}
+</style>
